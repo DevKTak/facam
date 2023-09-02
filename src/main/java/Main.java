265c7e53e7c5cfb433bab1fbf5d24863ee5bec68
ktpark <qkrrudxkr77@naver.com>
2023-09-02 14:43:32 +0900

    refactor: 코드 리팩토링

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -27,12 +26,12 @@ public class Main {
 	private static final String CATEGORY_GROUP_CODE = "PM9";
 	private static final int size = 10;
 
-	private static String buildUrlAndQueryStringByKeyword(final String keyword) {
+	private static String buildUrlConcatQueryStringByKeyword(final String keyword) {
 		String encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8);
 		return KAKAO_SEARCH_BASE_URL + "/keyword.json?query=" + encodedKeyword;
 	}
 
-	private static String buildUrlAndQueryStringByCategory(double[] coordinate, int radius) {
+	private static String buildUrlConcatQueryStringByCategory(double[] coordinate, int radius) {
 		return KAKAO_SEARCH_BASE_URL + "/category.json?category_group_code=" + CATEGORY_GROUP_CODE
 			+ "&x=" + coordinate[0] + "&y=" + coordinate[1]
 			+ "&radius=" + radius + "&size=" + size;
@@ -49,29 +48,27 @@ public class Main {
 
 		System.out.print(System.lineSeparator());
 
-		String requestKeywordUrl = buildUrlAndQueryStringByKeyword(keyword);
+		String requestKeywordUrl = buildUrlConcatQueryStringByKeyword(keyword);
 
 		// 키워드로 장소 검색해서 위도와 경도 얻기
 		double[] coordinate = getCoordinateByKeyword(requestKeywordUrl);
 
-		if (coordinate != null) {
-			String requestCategoryUrl = buildUrlAndQueryStringByCategory(coordinate, radius);
+		String requestCategoryUrl = buildUrlConcatQueryStringByCategory(coordinate, radius);
 
-			// 카테고리로 장소 검색해서 약국 정보 리스트 10개 얻기
-			List<ApiResponse.PlaceInfoResponse> placeInfoList = getPlaceInfoByCategory(requestCategoryUrl);
+		// 카테고리로 장소 검색해서 약국 정보 리스트 10개 얻기
+		List<ApiResponse.PlaceInfoResponse> placeInfoList = getPlaceInfoByCategory(requestCategoryUrl);
 
-			printPlaceInfo(keyword, radius, placeInfoList);
+		printPlaceInfo(keyword, radius, placeInfoList);
 
-			while (true) {
-				System.out.print("kakaomap URL(장소 URL): ");
-				String urlOrExit = br.readLine();
+		while (true) {
+			System.out.print("kakaomap URL(장소 URL): ");
+			String urlOrExit = br.readLine();
 
-				if ("exit".equals(urlOrExit)) {
-					System.out.println("프로그램 종료");
-					break;
-				} else {
-					openUrlInBrowser(urlOrExit.trim());
-				}
+			if ("exit".equals(urlOrExit)) {
+				System.out.println("프로그램 종료");
+				break;
+			} else {
+				openUrlInBrowser(urlOrExit.trim());
 			}
 		}
 	}
@@ -86,7 +83,7 @@ public class Main {
 		} else if (osName.contains("mac")) {
 			command = new String[] {"open", url};
 		} else {
-			System.out.println("운영체제가 지원되지 않습니다.");
+			System.out.println("해당 운영체제에서는 지원되지 않습니다.");
 			return;
 		}
 
@@ -95,6 +92,7 @@ public class Main {
 			processBuilder.start();
 		} catch (IOException e) {
 			System.err.println("URL을 열 수 없습니다: " + e);
+			throw new RuntimeException(e);
 		}
 	}
 
@@ -102,24 +100,27 @@ public class Main {
 	private static void printPlaceInfo(String keyword, int radius, List<ApiResponse.PlaceInfoResponse> placeInfoList) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("입력한 위치 키워드: ").append(keyword).append(System.lineSeparator());
-		sb.append("검색 반경: ")
-			.append(String.format("%.1fkm", radius / 1000d))
+		sb.append("검색 반경: ").append(String.format("%.1fkm", radius / 1000d))
 			.append(System.lineSeparator())
 			.append(System.lineSeparator());
 
 		for (ApiResponse.PlaceInfoResponse placeInfo : placeInfoList) {
-			sb.append("** 약국 검색 결과 **").append(System.lineSeparator());
-			sb.append("장소 URL(지도 위치): ").append(placeInfo.place_url()).append(System.lineSeparator());
-			sb.append("상호명: ").append(placeInfo.place_name()).append(System.lineSeparator());
-			sb.append("주소: ").append(placeInfo.address_name()).append(System.lineSeparator());
-			sb.append("전화번호: ").append(placeInfo.phone()).append(System.lineSeparator());
-			sb.append("거리(km): ")
-				.append(String.format("%.3fkm", placeInfo.distance() / 1000d))
-				.append(System.lineSeparator())
-				.append("----------------------------------------")
-				.append(System.lineSeparator());
+			buildSearchInfo(sb, placeInfo);
 		}
 		System.out.println(sb);
+	}
+
+	private static void buildSearchInfo(StringBuilder sb, ApiResponse.PlaceInfoResponse placeInfo) {
+		sb.append("** 약국 검색 결과 **").append(System.lineSeparator());
+		sb.append("장소 URL(지도 위치): ").append(placeInfo.place_url()).append(System.lineSeparator());
+		sb.append("상호명: ").append(placeInfo.place_name()).append(System.lineSeparator());
+		sb.append("주소: ").append(placeInfo.address_name()).append(System.lineSeparator());
+		sb.append("전화번호: ").append(placeInfo.phone()).append(System.lineSeparator());
+		sb.append("거리(km): ")
+			.append(String.format("%.3fkm", placeInfo.distance() / 1000d))
+			.append(System.lineSeparator())
+			.append("----------------------------------------")
+			.append(System.lineSeparator());
 	}
 
 	// 카테고리로 장소 검색하기
@@ -127,27 +128,32 @@ public class Main {
 		JsonProcessingException {
 		String responseBody = httpGet(requestCategoryUrl);
 
-		if (StringUtils.isNotBlank(responseBody)) {
-			ObjectMapper objectMapper = new ObjectMapper();
-			ApiResponse apiResponse = objectMapper.readValue(responseBody, ApiResponse.class);
-
-			return apiResponse.documents();
+		if (StringUtils.isBlank(responseBody)) {
+			throw new IllegalStateException("API 응답값이 존재하지 않습니다.");
 		}
-		return new ArrayList<>();
+		ObjectMapper objectMapper = new ObjectMapper();
+		ApiResponse apiResponse = objectMapper.readValue(responseBody, ApiResponse.class);
+
+		return apiResponse.documents();
 	}
 
 	// 키워드로 장소 검색하기
-	private static double[] getCoordinateByKeyword(String requestKeywordUrl) throws JsonProcessingException {
-		ObjectMapper objectMapper = new ObjectMapper();
-		String responseBody = httpGet(requestKeywordUrl);
+	private static double[] getCoordinateByKeyword(String requestKeywordUrl) {
+		JsonNode firstObject;
 
-		if (StringUtils.isNotBlank(responseBody)) {
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			String responseBody = httpGet(requestKeywordUrl);
+
+			if (StringUtils.isBlank(responseBody)) {
+				throw new IllegalStateException("API 응답값이 존재하지 않습니다.");
+			}
 			JsonNode rootNode = objectMapper.readTree(responseBody);
-			JsonNode firstObject = rootNode.get("documents").get(0);
-
-			return new double[] {firstObject.get("x").asDouble(), firstObject.get("y").asDouble()};
+			firstObject = rootNode.get("documents").get(0);
+		} catch (JsonProcessingException e) {
+			throw new RuntimeException(e);
 		}
-		return null;
+		return new double[] {firstObject.get("x").asDouble(), firstObject.get("y").asDouble()};
 	}
 
 	// HttpClient, HttpGet 객체 생성
@@ -158,16 +164,19 @@ public class Main {
 
 		try {
 			HttpResponse response = client.execute(getRequest);
+			int statusCode = response.getStatusLine().getStatusCode();
 
-			if (response.getStatusLine().getStatusCode() == 200) {
+			if (statusCode == 200) {
 				ResponseHandler<String> handler = new BasicResponseHandler();
+
 				return handler.handleResponse(response);
 			} else {
-				System.out.println("[API 호출 실패]\n 상태 코드: " + response.getStatusLine().getStatusCode());
+				System.err.println("[API 호출 실패]\n 상태 코드: " + statusCode);
+				throw new RuntimeException("API 호출 실패");
 			}
 		} catch (Exception e) {
 			System.err.println("[API 호출 오류]: " + e);
+			throw new RuntimeException("[API 호출 오류]: " + e);
 		}
-		return "";
 	}
 }

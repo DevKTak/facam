9a9e9197c0a97ee235a44fa44bb0819f00dd7914
rightpair <tkddn204@gmail.com>
2023-09-05 01:10:14 +0900

    add itinerary service

--- app/src/main/java/kdt_y_be_toy_project1/itinerary/service/ItineraryService.java
+++ app/src/main/java/kdt_y_be_toy_project1/itinerary/service/ItineraryService.java
@@ -1,25 +1,40 @@
 package kdt_y_be_toy_project1.itinerary.service;
 
+import kdt_y_be_toy_project1.itinerary.dao.ItineraryCSVDao;
+import kdt_y_be_toy_project1.itinerary.dao.ItineraryJSONDao;
 import kdt_y_be_toy_project1.itinerary.entity.Itinerary;
 import kdt_y_be_toy_project1.itinerary.type.FileType;
 
 import java.util.List;
 
+import static kdt_y_be_toy_project1.itinerary.type.FileType.JSON;
+
 public class ItineraryService {
+
+  private ItineraryJSONDao jsonDao = new ItineraryJSONDao();
+  private ItineraryCSVDao csvDao = new ItineraryCSVDao();
 
   List<Itinerary> getAllItineraryList(int tripId, FileType type) {
     // fileApi를 통해 Itinerary 객체 리스트를 받아온다
-
-    return null;
+    if (type.equals(JSON)) {
+      return jsonDao.getItineraryListFromFile(tripId);
+    } else {
+      return csvDao.getItineraryListFromFile(tripId);
+    }
   }
 
   Itinerary getItinerary(int tripId, int itineraryId, FileType type) {
     // fileApi를 통해 Itinerary 객체를 받아온다
-
-    return null;
+    if (type.equals(JSON)) {
+      return jsonDao.getItineraryFromFile(tripId, itineraryId);
+    } else {
+      return csvDao.getItineraryFromFile(tripId, itineraryId);
+    }
   }
 
-  void addItinerary(Itinerary itinerary) {
+  void addItinerary(int tripId, Itinerary itinerary) {
     // fileApi를 통해 Itinerary 객체를 넣는다
+    csvDao.addItineraryToFile(tripId, itinerary);
+    jsonDao.addItineraryToFile(tripId, itinerary);
   }
 }

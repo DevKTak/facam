098d3bd6482e69290936b33e1d7624c2372089f8
Nine-JH <rnwkgus2@gmail.com>
2023-09-10 01:44:52 +0900

    Add validating input code in `AppConsole`
    Motivation:
    
    - see 9abede38
    
    Modification:
    
    - add validating input code

--- app/src/main/java/kdt_y_be_toy_project1/view/ViewTemplate.java
+++ app/src/main/java/kdt_y_be_toy_project1/view/ViewTemplate.java
@@ -33,14 +33,16 @@ public final class ViewTemplate {
 
     public static final String TRIP_DETAIL_RESPONSE_HEADER = """
         ===========================================================================================================================
-           id         출발지         주소             출 발                 도 착                체 크 인                체 크 아 웃
+           id         출발지         도착지             출 발                 도 착                체 크 인                체 크 아 웃
         ===========================================================================================================================
         """;
 
     public static final String INSERT_ARGUMENT_DISPLAY = "%s을 입력하세요:\t";
-    public static final String INSERT_CORRECT_LOCAL_DATE_FORMAT_DISPLAY = "입력 포맷(yyyy-mm-dd)을 지켜주세요. (예: 2023-09-05)";
+    public static final String INSERT_CORRECT_LOCAL_DATE_FORMAT_DISPLAY = "입력 포맷(yyyy-MM-dd)을 지켜주세요. (예: 2023-09-05)";
+    public static final String INSERT_CORRECT_LOCAL_DATE_TIME_FORMAT_DISPLAY = "입력 포맷(yyyy-MM-dd HH:mm:ss)을 지켜주세요. (예: 2023-09-05 : 11:30:00)";
     public static final String SAVE_OR_NOT_DISPLAY = "저장하시겠습니까?(Y/N):\t";
     public static final String SAVE_DONE_DISPLAY = "저장완료!";
+    public static final String BLANK_NOT_REQUIRE = "공백은 허용하지 않습니다.";
     public static final String INSERT_ONLY_Y_OR_N_DISPLAY = "'Y' 또는 'N' 만 입력해주세요";
     public static final String SELECT_SAVE_FILE_FORMAT_DISPLAY = "어떤 파일로 저장하시겠습니까? (json, csv):\t";
     public static final String INSERT_ONLY_JSON_OR_CSV_DISPLAY = "'json' 또는 'csv' 만 입력해주세요";

749fe665ac6267c418816a400aa2d3a8cb784418
ktpark <qkrrudxkr77@naver.com>
2023-10-13 11:04:20 +0900

    no message

--- src/main/java/com/book/dto/ApiResponse.java
+++ src/main/java/com/book/dto/ApiResponse.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+// @JsonIgnoreProperties(ignoreUnknown = true): Jackson 라이브러리를 사용하여 JSON을 Java 객체로 변환할 때,
+// 알려지지 않은 속성들을 무시하도록 지정하는 어노테이션
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ApiResponse(List<BookResponse> documents) {
 
